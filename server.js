e37615dfd99a651e5ec4965715a58fb3046674de
Robert Tassy <Rtass001@fiu.edu>
2016-04-05 12:36:54 -0400

    reverting

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ var PORT = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/', function(request, response) {
-//   response.sendFile(__dirname + '/build/index.html')
-// });
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/build/index.html')
+});
 
 app.listen(PORT, function() {
   console.log("Server is up and running on port: " + PORT)
