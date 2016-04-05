68dcc254302b407738534492d5d66f2ff3021306
Robert Tassy <Rtass001@fiu.edu>
2016-04-05 17:10:19 -0400

    added ; deploy test

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var PORT = process.env.PORT || 8080
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/build/index.html')
+  response.sendFile(__dirname + '/build/index.html');
 });
 
 app.listen(PORT, function() {
-  console.log("Server is up and running on port: " + PORT)
+  console.log("Server is up and running on port: " + PORT);
 });
